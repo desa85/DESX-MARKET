18d80d99bc678eeee75e25381bc8aea3ff1af516
nmdaniil <behelit300@gmail.com>
2021-11-18 15:37:13 +0300

    Updates

---
+++ precache-manifest.97698004b0ee23ab1f776b4bdb2e8672.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3359b9aa3f2176f06509",
+    "url": "/DESX-MARKET/static/css/main.107841c1.chunk.css"
+  },
+  {
+    "revision": "3359b9aa3f2176f06509",
+    "url": "/DESX-MARKET/static/js/main.3359b9aa.chunk.js"
+  },
+  {
+    "revision": "5dad7e04a4ba5e44e487",
+    "url": "/DESX-MARKET/static/js/1.5dad7e04.chunk.js"
+  },
+  {
+    "revision": "6f29eb60cb93baf1a211",
+    "url": "/DESX-MARKET/static/js/runtime~main.6f29eb60.js"
+  },
+  {
+    "revision": "dde5e7e352e935bb8fb55907f8304c15",
+    "url": "/DESX-MARKET/static/media/currency.dde5e7e3.svg"
+  },
+  {
+    "revision": "09d4897ce70bf22129c03629c176d731",
+    "url": "/DESX-MARKET/static/media/background.09d4897c.jpg"
+  },
+  {
+    "revision": "f7fc10927a6209cdaa1857d5694efade",
+    "url": "/DESX-MARKET/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/DESX-MARKET/precache-manifest.97698004b0ee23ab1f776b4bdb2e8672.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/DESX-MARKET/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
